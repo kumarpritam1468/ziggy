dd0f49a4bfbde8ff2ca6fe4df3647c4bd10701ad
Pritam <manoharipritam@gmail.com>
2024-07-16 20:12:19 +0530

    add full UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import Home from './pages/Home/Home'
+import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Cart from './pages/Cart/Cart'
+import LoginPopup from './components/LoginPopup/LoginPopup'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import MyOrders from './pages/MyOrders/MyOrders'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [showLogin,setShowLogin] = useState(false);
 
   return (
     <>
-      Welcome to my app
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
+      <div className='app'>
+        <Navbar setShowLogin={setShowLogin}/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/order' element={<PlaceOrder/>}/>
+          <Route path='/myorder' element={<MyOrders/>}/>
+        </Routes>
+      </div>
+      <Footer />
     </>
   )
 }
